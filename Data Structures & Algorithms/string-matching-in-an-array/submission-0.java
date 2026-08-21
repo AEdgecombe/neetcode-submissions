6cf78892ff65a3a7b0c9c1610daae679bbdf14df
Alexander Michael Edgecombe <109439961+AEdgecombe@users.noreply.github.com>
2026-08-21 13:44:37 +0530

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        // Created an array list with the same order as words
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            arrayList.add(words[i]);
+        }
+
+        // Create the array list solution
+        ArrayList<String> solution = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i].toLowerCase();
+
+            for (int j = 0; j < arrayList.size(); j++) {
+                String listWord = arrayList.get(j).toLowerCase();
+                if (i != j) {
+                    if (listWord.contains(currentWord)) {
+                        solution.add(currentWord);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return solution;
+    }
+}
