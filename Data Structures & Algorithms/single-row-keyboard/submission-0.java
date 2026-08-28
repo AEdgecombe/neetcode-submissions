da42dc9c15ffc83e285f3f1a10b808f76836064a
Alexander Michael Edgecombe <109439961+AEdgecombe@users.noreply.github.com>
2026-08-28 09:52:40 +0530

    Add: single-row-keyboard - submission-0

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int calculateTime(String keyboard, String word) {
+        int value = 0;
+        int total = 0;
+
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        char[] array = keyboard.toCharArray();
+        char[] wordArray = word.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            hashMap.put(array[i], i);
+        }
+
+        for (int i = 0; i < wordArray.length; i++) {
+            int letterValue = hashMap.get(wordArray[i]);
+            total = total + Math.abs(letterValue - value);
+            value = letterValue;
+        }
+
+        return total;
+    }
+}
