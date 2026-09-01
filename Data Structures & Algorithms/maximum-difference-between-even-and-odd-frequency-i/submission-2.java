1b5bd56db4028ef7c057073f55565e6cdb3da522
Alexander Michael Edgecombe <109439961+AEdgecombe@users.noreply.github.com>
2026-09-01 11:38:48 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-2

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxDifference(String s) {
+        int maxOdd = 0;
+        int minEven = 101;
+
+        char[] array = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (Character c : array) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> pairs : map.entrySet()) {
+            if (pairs.getValue() % 2 == 0) {
+                if (pairs.getValue() < minEven) {
+                    minEven = pairs.getValue();
+                }
+            } else if (pairs.getValue() % 2 != 0) {
+                if (pairs.getValue() > maxOdd) {
+                    maxOdd = pairs.getValue();
+                }
+            } else {
+                return 0;
+            }
+        }
+
+        int solution = maxOdd - minEven;
+        return solution;
+    }
+}
