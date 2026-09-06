5337dabcd802bb6175a6d57ed828f169658b96e1
Alexander Michael Edgecombe <109439961+AEdgecombe@users.noreply.github.com>
2026-09-06 18:00:15 +0530

    Add: first-unique-character-in-a-string - submission-0

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        // chars into a linked hashset and index of first false
+
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        char[] array = s.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            map.put(array[i], map.getOrDefault(array[i], 0) + 1);
+        }
+        
+        for (int i = 0; i < array.length; i++) {
+            if (map.get(array[i]) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
